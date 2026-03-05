067334ef208eaf3d0c77026c47bd5324ce924637
crespitosoff <jhoseb2006.com@gmail.com>
2026-03-05 01:20:46 -0500

    Agregando Gson, y corriendo el programa

---
+++ src/lad/com/alura/conversormoneda/Conversor.java
@@ -0,0 +1,20 @@
+package lad.com.alura.conversormoneda;
+
+public class Conversor {
+    public static void eleccionUserMenu() {
+        System.out.print("""
+                Sea bienvenido/a al Conversor de Moneda =]
+                
+                ==================================================
+                1) Dólar        =>> P argentino
+                2) P argentino  =>> Dólar
+                3) Dólar        =>> R brasileño
+                4) R brasileño  =>> Dólar
+                5) Dólar        =>> P colombiano
+                6) P colombiano =>> Dólar
+                7) Salir
+                ==================================================
+                Elija una opción válida:
+                """);
+    }
+}
---
+++ src/lad/com/alura/conversormoneda/ConversorApp.java
@@ -0,0 +1,95 @@
+package lad.com.alura.conversormoneda;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class ConversorApp {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+
+        int opcion = 0;
+
+
+        while (opcion != 7) {
+            Conversor.eleccionUserMenu();
+            opcion = sc.nextInt();
+
+            System.out.println("Ingrese el valor que desea convertir: ");
+            double cantidad = sc.nextDouble();
+
+            switch (opcion) {
+                case 1:
+                    double P_ARG = obtenerTasa("USD", "ARS", String.valueOf(cantidad));
+                    System.out.println(P_ARG);
+                    break;
+                case 2:
+                    double DOLAR = obtenerTasa("ARS", "USD", String.valueOf(cantidad));
+                    System.out.println(DOLAR);
+                    break;
+                case 3:
+                    double R_BRA = obtenerTasa("USD", "BRL", String.valueOf(cantidad));
+                    System.out.println(R_BRA);
+                    break;
+                case 4:
+                    double USD = obtenerTasa("BRL", "USD", String.valueOf(cantidad));
+                    System.out.println(USD);
+                    break;
+                case 5:
+                    double P_COL = obtenerTasa("USD", "COP", String.valueOf(cantidad));
+                    System.out.println(P_COL);
+                    break;
+                case 6:
+                    double COL = obtenerTasa("COP", "USD", String.valueOf(cantidad));
+                    System.out.println(COL);
+                    break;
+                case 7:
+                    System.out.println("Gracias por usar el conversor.");
+                    opcion = 7;
+                    break;
+            }
+            System.out.println("\n");
+        }
+        sc.close();
+    }
+
+    public static double obtenerTasa(String base_rate, String target_rate, String amount) throws IOException, InterruptedException {
+        // Creando cliente HTTP
+        HttpClient cliente = HttpClient.newHttpClient();
+
+        // Creando URL
+        String url = "https://v6.exchangerate-api.com/v6/9ba310889b4d07769a662fc0/pair/" + base_rate + "/" + target_rate + "/" + amount;
+
+        // Creando la solicitud
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        try {
+            // Enviando solicitud y recibiendo respuesta
+            HttpResponse<String> respuesta = cliente.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Conversión a JSON
+            JsonElement elemento = JsonParser.parseString(respuesta.body());
+            JsonObject objectRoot = elemento.getAsJsonObject();
+
+            //Accediendo a JsonObject
+            return objectRoot.get("conversion_result").getAsDouble();
+        } catch (IOException e) {
+            throw new IOException(e.getMessage());
+        } catch (InterruptedException e) {
+            throw new InterruptedException(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error desconocido: " + e.getMessage());
+        }
+        return 0;
+    }
+}
